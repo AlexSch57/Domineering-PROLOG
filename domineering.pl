2cc1521b68781627654155b831f83661a2b5d273
Y.T <38046994+vesran@users.noreply.github.com>
2019-04-02 13:39:06 +0200

    Update domineering.pl

--- domineering.pl
+++ domineering.pl
@@ -31,3 +31,10 @@ creerListe([-|L], X) :-
  %   suivant(X, Y),
   %  ajoute_debut(-, L, L),
    % creerListe(L, Y).
+
+
+initListe([b], X) :- ligne(X).
+initListe([a|L], X) :-
+    \+ligne(X),
+    suivant(X, Y),
+    initListe(L, Y).
