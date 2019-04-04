bf2e6d06c36d830bb882f4c28d7d6fad847c4c1a
Y.T <38046994+vesran@users.noreply.github.com>
2019-04-04 20:14:02 +0200

    Placer des pièces est devenu possible
    Bugs pour sortir du jeu causés par la fonction remplacer( )

--- domineering.pl
+++ domineering.pl
@@ -10,22 +10,22 @@ vertical(joueur1, X, Y) :- turn(joueur1), ligne(X), colonne(Y) .
 ligne(6).
 colonne(6).
 
-suivant(0, 1).
 suivant(1, 2).
 suivant(2, 3).
 suivant(3, 4).
 suivant(4 ,5).
 suivant(5, 6).
+suivant(6, 7).  % La fonction remplacer( ) marche s'il y a un nombre suivant la taille maximale.
 
-initLigne([-], X) :- 
+initLigne([-], X) :-
     ligne(X).
 initLigne([-|L], X) :-
     \+ligne(X),
     suivant(X, Y),
     initLigne(L, Y).
 
-initColonne([L1], X) :- 
-    colonne(X), 
+initColonne([L1], X) :-
+    colonne(X),
     initLigne(L1, 1).
 initColonne([L1|L], X) :-
     \+colonne(X),
@@ -36,23 +36,72 @@ initColonne([L1|L], X) :-
 init(L) :- initColonne(L, 1), afficherTableau(L).
 
 afficherTableau([]).
-afficherTableau([X|L]) :- 
-    writeln(X), 
+afficherTableau([X|L]) :-
+    writeln(X),
     afficherTableau(L).
-    
-  
+
+
 description :-
     writeln("Description du jeu et des regles blablabla...").
-    
+
 lancerJeu :-
     description,
     init(Plateau),
-    jouer(Plateau).
-    
-jouer(Plateau) :-
-    afficherTableau(Plateau),
-    write("Case : "),
-    read(PositionLigne),
-    read(PositionColonne),
-    %placerH(Plateau, PositionLigne, PositionColonne, NewPlateau),
-    jouer(NewPlateau).
+    jouer(Plateau, joueur1).
+
+jouer(Plateau, joueur1) :-
+    write("Case X: "),
+    read(X),
+    write("Case Y :"),
+    read(Y),
+    placementHorizontal(o, X, Y, Plateau, NouveauPlateau),
+    afficherTableau(NouveauPlateau),
+    jouer(NouveauPlateau, joueur2).
+
+jouer(Plateau, joueur2) :-
+    write("Case X: "),
+    read(X),
+    write("Case Y :"),
+    read(Y),
+    placementVertical(x, X, Y, Plateau, NouveauPlateau),
+    afficherTableau(NouveauPlateau),
+    jouer(NouveauPlateau, joueur1).
+
+
+
+
+placementHorizontal(Pion, X, Y, Plateau, NouveauPlateau) :-
+    placer(Pion, X, Y, Plateau, PlateauIntermediaire),
+    suivant(Y, YSuiv),
+    placer(Pion, X, YSuiv, PlateauIntermediaire, NouveauPlateau).
+
+placementVertical(Pion, X, Y, Plateau, NouveauPlateau) :-
+    placer(Pion, X, Y, Plateau, PlateauIntermediaire),
+    suivant(X, XSuiv),
+    placer(Pion, XSuiv, Y, PlateauIntermediaire, NouveauPlateau).
+
+
+placer(Pion, X, Y, Plateau, NouveauPlateau) :-
+    extraire(X, 1, Plateau, ListeX),
+    %writeln(ListeX),
+    remplacer(Y, 1, ListeX, Pion, NouvelleListeX),
+    %writeln(NouvelleListeX),
+    remplacer(X, 1, Plateau, NouvelleListeX, NouveauPlateau).
+
+
+extraire(Position, Position, [ElementExtraite | _], ElementExtraite).
+extraire(Position, X, [_ | Liste], ElementExtraite) :-
+    suivant(X, XSuiv),
+    extraire(Position, XSuiv, Liste, ElementExtraite).
+
+
+% Ne se termine pas en 1 coup et creer des bugs pour quitter le jeu! -->
+% a corriger Remplacer l'element qui se trouve a l'index indique.
+remplacer(_, _, [], _, []).
+remplacer(Position, Position, [_ | Liste], Element, [Element | CopieReste]) :-
+    suivant(Position, PSuiv),
+    remplacer(Position, PSuiv, Liste, Element, CopieReste).
+remplacer(Position, Y, [Tete | Liste], Element, [Tete | CopieReste]) :-
+    suivant(Y, YSuiv),
+    remplacer(Position, YSuiv, Liste, Element, CopieReste).
+
