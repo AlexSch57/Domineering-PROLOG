e879c07029018c00a459d6101cc6ba9243e5398e
Y.T <38046994+vesran@users.noreply.github.com>
2019-04-11 17:54:57 +0200

    Vérification des emplacements libres

--- domineering.pl
+++ domineering.pl
@@ -1,5 +1,5 @@
-joueur(joueur1) .
-joueur(joueur2) .
+joueur(joueur1).
+joueur(joueur2).
 
 ligne(6).
 colonne(6).
@@ -27,7 +27,9 @@ initColonne([L1|L], X) :-
     initLigne(L1, 1),
     initColonne(L, Y).
 
-init(L) :- initColonne(L, 1), afficherTableau(L).
+init(L) :-
+    initColonne(L, 1),
+    afficherTableau(L).
 
 afficherTableau([]) :-
     writeln("").
@@ -45,6 +47,7 @@ lancerJeu :-
     jouer(Plateau, joueur1).
 
 jouer(Plateau, joueur1) :-
+    peutJouerHorizontal(Plateau, 1, 1),
     writeln("Joueur 1 : "),
     write("Ligne : "),
     read(X),
@@ -55,6 +58,7 @@ jouer(Plateau, joueur1) :-
     jouer(NouveauPlateau, joueur2).
 
 jouer(Plateau, joueur2) :-
+    peutJouerVertical(Plateau, 1, 1),
     writeln("Joueur 2 : "),
     write("Ligne : "),
     read(X),
@@ -64,11 +68,21 @@ jouer(Plateau, joueur2) :-
     afficherTableau(NouveauPlateau),
     jouer(NouveauPlateau, joueur1).
 
-           
+% Arret du jeu
+jouer(Plateau, joueur1) :-
+    \+peutJouerHorizontal(Plateau, 1, 1),
+    writeln("Joueur 1 a perdu !.").
+
+jouer(Plateau, joueur2) :-
+    \+peutJouerVertical(Plateau, 1, 1),
+    writeln("Joueur 2 a perdu !").
+
+
+
 placementHorizontal(Pion, X, Y, Plateau, NouveauPlateau, joueur1) :-
     placer(Pion, X, Y, Plateau, PlateauIntermediaire),
     \+colonne(Y),
-    suivant(Y, YSuiv), 
+    suivant(Y, YSuiv),
     placer(Pion, X, YSuiv, PlateauIntermediaire, NouveauPlateau).
 
 placementHorizontal(_, _, _, Plateau, _, joueur1) :-
@@ -87,19 +101,19 @@ placementVertical(_, _, _, Plateau, _, joueur2) :-
     afficherTableau(Plateau),
 	jouer(Plateau, joueur2).
 
+% Verifie si la case a la position indiquee est libre.
 libre(X, Y, Plateau) :-
     extraire(X, 1, Plateau, Liste),
     extraire(Y, 1, Liste, -).
 
+% Creer un nouveau plateau avec la case indiquee occupee.
 placer(Pion, X, Y, Plateau, NouveauPlateau) :-
-    extraire(X, 1, Plateau, ListeX),   
+    extraire(X, 1, Plateau, ListeX),
     libre(X, Y, Plateau),
-    %writeln(ListeX),
     remplacer(Y, 1, ListeX, Pion, NouvelleListeX),
-    %writeln(NouvelleListeX),
     remplacer(X, 1, Plateau, NouvelleListeX, NouveauPlateau).
 
-
+% Extrait un element d'une liste a l'index voulu.
 extraire(Position, Position, [ElementExtraite | _], ElementExtraite).
 extraire(Position, X, [_ | Liste], ElementExtraite) :-
     suivant(X, XSuiv),
@@ -116,4 +130,59 @@ remplacer(Position, Position, [_ | Liste], Element, [Element | CopieReste]) :-
 remplacer(Position, Y, [Tete | Liste], Element, [Tete | CopieReste]) :-
     suivant(Y, YSuiv),
     \+Position == Y,
-    remplacer(Position, YSuiv, Liste, Element, CopieReste).
+remplacer(Position, YSuiv, Liste, Element, CopieReste).
+
+
+% Verifie si on peut placer une piece verticale sur le plateau
+% a la position indiquee.
+emplacementVerticalLibre(Plateau, X, Y) :-
+    \+ligne(X),
+    suivant(X, XSuiv),
+    libre(X, Y, Plateau),
+    libre(XSuiv, Y, Plateau).
+
+emplacementHorizontalLibre(Plateau, X, Y) :-
+    \+colonne(Y),
+    suivant(Y, YSuiv),
+    libre(X, Y, Plateau),
+    libre(X, YSuiv, Plateau).
+% Verifie si on peut mettre une piece verticale
+% quelquepart sur le plateau.
+peutJouerVertical(Plateau, X, Y) :-
+    \+ligne(X),
+    emplacementVerticalLibre(Plateau, X, Y).
+
+peutJouerVertical(Plateau, X, Y) :-
+    \+ligne(X),
+    \+colonne(Y),
+    suivant(Y, YSuiv),
+    \+emplacementVerticalLibre(Plateau, X, Y),
+    peutJouerVertical(Plateau, X, YSuiv).
+
+peutJouerVertical(Plateau, X, Y) :-
+    \+ligne(X),
+    colonne(Y),
+    \+emplacementVerticalLibre(Plateau, X, Y),
+    suivant(X, XSuiv),
+    peutJouerVertical(Plateau, XSuiv, 1).
+
+
+% Verifie si on peut mettre une piece horizontale
+% quelquepart sur le plateau.
+peutJouerHorizontal(Plateau, X, Y) :-
+    \+colonne(Y),
+    emplacementHorizontalLibre(Plateau, X, Y).
+
+peutJouerHorizontal(Plateau, X, Y) :-
+    \+colonne(Y),
+    \+ligne(X),
+    \+emplacementHorizontalLibre(Plateau, X, Y),
+    suivant(X, XSuiv),
+    peutJouerHorizontal(Plateau, XSuiv, Y).
+
+peutJouerHorizontal(Plateau, X, Y) :-
+    \+colonne(Y),
+    ligne(X),
+    \+emplacementHorizontalLibre(Plateau, X, Y),
+    suivant(Y, YSuiv),
+    peutJouerHorizontal(Plateau, 1, YSuiv).
